8918a46b83a4d7b9bd2642b20303c7fdad367c16
vajgi90 <vajgi90@gmail.com>
2020-10-07 12:33:05 +0200

    This is a small progrmam that uses random, os and json packages

---
+++ randjsongenerator.py
@@ -0,0 +1,17 @@
+import random
+import os
+import json
+
+count = int(os.getenv("FILE_COUNT") or 100)
+words = [word.strip() for word in open('/usr/share/dict/words').readlines()]
+
+
+for identifier in range(count):
+    amount = random.uniform(1.0, 1000)
+    content = {
+        'topic': random.choice(words),
+        'value': "%.2f" % amount
+    }
+    # The path is optional
+    with open(f'./new/receipt-{identifier}.json', 'w') as f:
+        json.dump(content, f)
